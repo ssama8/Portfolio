8d8797aa675152af686365c2f870d9f47ef47551
ssama8 <saketsama8@gmail.com>
2022-04-18 20:45:36 -0700

    React.lazy didn't make sense for this project and decided to remove it

--- src/App.js
+++ src/App.js
@@ -1,64 +1,29 @@
-import React from "react";
+import Home from "./pages/Home";
 import { useGlobalContext } from "./context/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-const Navbar = React.lazy(() => import("./Components/Navbar"));
-const Sidebar = React.lazy(() => import("././Components/Sidebar"));
-const Footer = React.lazy(() => import("./Components/Footer"));
-const Error = React.lazy(() => import("./pages/Error"));
-const Home = React.lazy(() => import("./pages/Home"));
-const ProjectsShowcase = React.lazy(() => import("./pages/ProjectsShowcase"));
-const About = React.lazy(() => import("./pages/About"));
-const HamburgerMenu = React.lazy(() => import("./Components/Hamburger"));
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import Footer from "./Components/Footer";
+import Error from "./pages/Error";
+import ProjectsShowcase from "./pages/ProjectsShowcase";
+import About from "./pages/About";
+import HamburgerMenu from "./Components/Hamburger";
 function App() {
 	const { theme } = useGlobalContext();
 	const htmlObj = document.documentElement;
 	htmlObj.className = theme;
 	return (
 		<Router>
-			<React.Suspense fallback={<>...</>}>
-				<Navbar />
-			</React.Suspense>
-			<React.Suspense fallback={<>...</>}>
-				<Sidebar />
-			</React.Suspense>
-			<React.Suspense fallback={<>...</>}>
-				<HamburgerMenu />
-			</React.Suspense>
+			<Navbar />
+			<Sidebar />
+			<HamburgerMenu />
 			<Routes>
-				<Route
-					path='/'
-					element={
-						<React.Suspense fallback={<>...</>}>
-							<Home />
-						</React.Suspense>
-					}
-				/>
-				<Route
-					path='/projects/:id'
-					element={
-						<React.Suspense fallback={<>...</>}>
-							<ProjectsShowcase />
-						</React.Suspense>
-					}></Route>
-				<Route
-					path='/projects/:id'
-					element={
-						<React.Suspense fallback={<>...</>}>
-							<About />
-						</React.Suspense>
-					}></Route>
-				<Route
-					path='/projects/:id'
-					element={
-						<React.Suspense fallback={<>...</>}>
-							<Error />
-						</React.Suspense>
-					}></Route>
+				<Route path='/' element={<Home />} />
+				<Route path='/projects/:id' element={<ProjectsShowcase />}></Route>
+				<Route path='/about' element={<About />} />
+				<Route path='*' element={<Error />}></Route>
 			</Routes>
-			<React.Suspense fallback={<>...</>}>
-				<Footer />
-			</React.Suspense>
+			<Footer />
 		</Router>
 	);
 }
